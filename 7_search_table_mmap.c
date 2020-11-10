7ce4087ce384c5da9ee5719a0ad598a373a8de5b
Gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-11-10 17:53:09 +0700

    Update 7_search_table_mmap.c

--- 7_search_table_mmap.c
+++ 7_search_table_mmap.c
@@ -57,8 +57,8 @@ int print_table(int fd, Line_entry *table, char *map, unsigned int table_size) {
     timeout.tv_sec = 5;
     timeout.tv_usec = 0;
     int fd_amount = select(max_fd + 1, &rfds, NULL, NULL, &timeout);
-    if (FD_ISSET(fd, &rfds) != 0) {
-        if (fd_amount > 0) {
+    if (fd_amount > 0) {
+        if (FD_ISSET(fd, &rfds) != 0) {
             while (true) {
                 printf("print line number: ");
                 scanf("%d", &scan_line);
@@ -68,28 +68,28 @@ int print_table(int fd, Line_entry *table, char *map, unsigned int table_size) {
                 }
                 if (scan_line == 0)
                     break;
-                print_line(table[scan_line], map);
+                print_line(fd, table[scan_line]);
                 putchar('\n');
             }
+        } else {
+            printf("fd absent in fd_set");
         }
-        if (fd_amount == -1) {
-            fprintf(stderr, "Error at select()\n");
-            return -1;
+    }
+    if (fd_amount == -1) {
+        fprintf(stderr, "Error at select()\n");
+        return -1;
+    }
+    if (fd_amount == 0) {
+        printf("Time is up! Keep the whole file:\n");
+        for (unsigned int i = 1; i < table_size; i++) {
+            print_line(fd, table[i]);
+            putchar('\n');
         }
-        if (fd_amount == 0) {
-            printf("Time is up! Keep the whole file:\n");
-            for (unsigned int i = 1; i < table_size; i++) {
-                print_line(table[i], map);
-                putchar('\n');
-            }
-        }
-        close(fd);
-        if (close(fd) == -1) {
-            fprintf(stderr, "Close error\n");
-            return -1;
-        }
-    } else {
-        printf("fd absent in fd_set");
+    }
+    close(fd);
+    if (close(fd) == -1) {
+        fprintf(stderr, "Close error\n");
+        return -1;
     }
     return 0;
 }
