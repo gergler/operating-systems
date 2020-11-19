0c5750476846e57bdd7a0629d7a7b321e473019b
Gerglerd <54989664+Gerglerd@users.noreply.github.com>
2020-11-19 20:47:13 +0700

    Create 9_child_proccess.c

---
+++ 9_child_proccess.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    int status;
+    if (argc != 2) {
+        fprintf(stderr, "No file\n");
+        return EINVAL;
+    }
+    pid_t pid = fork();
+    if (pid == -1) {
+        fprintf(stderr, "Error at fork()");
+        return -1;
+    }
+    if (pid == 0) {
+        execl("/bin/cat", "cat", argv[1], NULL);
+        _exit(-1);
+    }
+    waitpid(pid, &status, 0);
+    if (WIFEXITED(status)) {
+        printf("Everything is okay\n");
+        return 1;
+    } else {
+        fprintf(stderr, "Error at terminating child process\n");
+        return -1;
+    }
+}
