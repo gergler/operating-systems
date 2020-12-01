e2bca4aafd338747a656a3b07715c2ef4116f40b
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-12-01 18:15:06 +0700

    Update 27_popen_wc.c

--- 27_popen_wc.c
+++ 27_popen_wc.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 
 int main(int argc, char *argv[]) {
-    int status;
-    char count[3];
+    int status, count;
     FILE *pipe = popen("grep \"^$\" ./text.txt | wc -l ", "r");
     if (pipe == NULL) {
         fprintf(stderr, "Error at pipe");
         return -1;
     }
-    fgets(count, 3, pipe);
+    if (EOF == fscanf(pipe, "%d", &count)) {
+        fprintf(stderr, "Error at scanf");
+        status = pclose(pipe);
+        if (status == -1) {
+            fprintf(stderr, "Error at pipe closing");
+            return -1;
+        } else if (WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "Child process ended with bad exit code: %d\n", WEXITSTATUS(status));
+            return -1;
+        } else if (WIFSIGNALED(status) != 0) {
+            fprintf(stderr, "Child ended due to signal: %d\n", WTERMSIG(status));
+            return -1;
+        }
+    }
+    printf("count of blank lines: %d\n", count);
     status = pclose(pipe);
     if (status == -1) {
         fprintf(stderr, "Error at pipe closing");
@@ -21,6 +35,5 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Child ended due to signal: %d\n", WTERMSIG(status));
         return -1;
     }
-    printf("count of blank lines: %s\n", count);
     return 0;
 }
