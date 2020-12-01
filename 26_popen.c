4a8dd5ef09554bbe1d3ed72fe8124d1a2a6688d1
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-12-01 17:46:59 +0700

    Update 26_popen.c

--- 26_popen.c
+++ 26_popen.c
@@ -4,8 +4,30 @@
 
 #define BUF_SIZE 100
 
-int pipe_close(FILE *pipe) {
-    int status = pclose(pipe);
+int main(int argc, char *argv[]) {
+    int status;
+    char buf[BUF_SIZE];
+    FILE *pipe = popen("echo text", "r");
+    if (pipe == NULL) {
+        fprintf(stderr, "Error at pipe");
+        return -1;
+    }
+    unsigned int nbytes = fread(buf, 1, BUFSIZ, pipe);
+    if (nbytes == -1) {
+        fprintf(stderr, "Error at fread");
+        status = pclose(pipe);
+        if (status == -1) {
+            fprintf(stderr, "Error at pipe closing");
+            return -1;
+        } else if (WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "Child process ended with bad exit code: %d\n", WEXITSTATUS(status));
+            return -1;
+        } else if (WIFSIGNALED(status) != 0) {
+            fprintf(stderr, "Child ended due to signal: %d\n", WTERMSIG(status));
+            return -1;
+        }
+    }
+    status = pclose(pipe);
     if (status == -1) {
         fprintf(stderr, "Error at pipe closing");
         return -1;
@@ -16,25 +38,9 @@ int pipe_close(FILE *pipe) {
         fprintf(stderr, "Child ended due to signal: %d\n", WTERMSIG(status));
         return -1;
     }
-}
-
-int main(int argc, char *argv[]) {
-    int status;
-    char buf[BUF_SIZE];
-    FILE *pipe = popen("echo text", "r");
-    if (pipe == NULL) {
-        fprintf(stderr, "Error at pipe");
-        return -1;
-    }
-    unsigned int read_bytes = fread(buf, 1, BUFSIZ, pipe);
-    if (read_bytes == -1) {
-        fprintf(stderr, "Error at fread");
-        pipe_close(pipe);
-    }
-    pipe_close(pipe);
-    for (int i = 0; i < read_bytes; i++)
+    for (int i = 0; i < nbytes; i++)
         buf[i] = (char) toupper(buf[i]);
-    if (fwrite(buf, read_bytes, 1, stdout) == -1) {
+    if (fwrite(buf, nbytes, 1, stdout) == -1) {
         fprintf(stderr, "Error at fwrite");
         return -1;
     }
