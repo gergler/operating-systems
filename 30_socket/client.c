d26fc4c9ff35318f55f30c8316b55008c8ecf210
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2020-12-22 12:07:44 +0700

    Update client.c

--- 30_socket/client.c
+++ 30_socket/client.c
@@ -1,37 +1,84 @@
-#include <sys/socket.h>
-#include <string.h>
-#include <sys/un.h>
-#include <zconf.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#include <sys/time.h>
+#include <zconf.h>
+#include <ctype.h>
 
-#define TEXT "Hello EveRYoNe!"
+#define SOCKETNAME  "mysocket"
 
-int main(int argc, char *argv[]) {
-    int sock;
+int main() {
+    int sock, msg_sock;
     int addr_len;
+
+    int read_num;
+    char buf[1024];
+
+    int maxfd, ready_fd;
+    fd_set fds;
 
     struct sockaddr_un name;
 
-    if (argc < 2) {
-        printf("Usage: %s <pathname>\n", argv[0]);
-        return -1;
-    }
+    unlink(SOCKETNAME);
 
     if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
         fprintf(stderr, "Error at opening socket\n");
         return -1;
     }
-
+    memset(&name, 0, sizeof(struct sockaddr_un));
     name.sun_family = AF_UNIX;
-    strcpy(name.sun_path, argv[1]);
+    strcpy(name.sun_path, SOCKETNAME);
     addr_len = sizeof(name.sun_family) + strlen(name.sun_path);
 
-    if (connect(sock, (struct sockaddr *) &name, addr_len) < 0)
-        fprintf(stderr, "Error at connect()\n");
+    if (bind(sock, (struct sockaddr *) &name, addr_len) < 0) {
+        fprintf(stderr, "Error at bind()\n");
+        return -1;
+    }
 
-    if (write(sock, TEXT, sizeof(TEXT)) != strlen(TEXT))
-        fprintf(stderr, "Error at write()\n");
+    printf("Socket name: %s\n", name.sun_path);
 
-    if (close(sock) == -1)
+    if (listen(sock, 5) < 0) {
+        fprintf(stderr, "Error at listen()\n");
+        return -1;
+    }
+
+    if ((msg_sock = accept(sock, 0, 0) < -1))
+        fprintf(stderr, "Error at accept() msg_socket\n");
+
+    maxfd = msg_sock + 1;
+    while (1) {
+        FD_ZERO(&fds);
+        FD_SET(msg_sock, &fds);
+
+        ready_fd = select(maxfd, &fds, (fd_set *) 0, (fd_set *) 0, (struct timeval *) 0);
+
+        if (ready_fd > 0) {
+            if (FD_ISSET(msg_sock, &fds) != 0) {
+                read_num = read(msg_sock, buf, sizeof(buf));
+                if (read_num < 1) {
+                    fprintf(stderr, "Can't recv\n");
+                    break;
+                }
+                if (read_num == 0) {
+                    printf("End connection\n");
+                    break;
+                } else {
+                    for (int i = 0; i < strlen(buf); i++)
+                        buf[i] = (char) toupper(buf[i]);
+                }
+            }
+        }
+        if (ready_fd == -1)
+            fprintf(stderr, "Error at select()\n");
+        if (close(msg_sock) == -1) {
+            fprintf(stderr, "Error at close msg_socket\n");
+            break;
+        }
+    }
+    if (close(sock) == -1) {
         fprintf(stderr, "Error at close socket\n");
+        return -1;
+    }
 }
+
