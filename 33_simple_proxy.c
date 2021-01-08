75014a93a16454768cb3552b516d45ec1cbc6525
Sayfudinova Polina <54989664+gergler@users.noreply.github.com>
2021-01-08 09:29:44 +0700

    Update 33_simple_proxy.c

--- 33_simple_proxy.c
+++ 33_simple_proxy.c
@@ -7,13 +7,14 @@
 #include <netdb.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 4096
 #define LISTEN  10
 #define PORT "5000"
 #define LISTEN_PORT "5000"
 
-static void wait_for_child(int sig) {
+static void sigaction_handler(int sig) {
     while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
@@ -40,26 +41,40 @@ void handle(int client) {
     fd_set set;
     unsigned int max_sock;
 
+    sigset_t mask, old_mask;
+    struct sigaction act;
+
+    memset (&act, 0, sizeof(act));
+    act.sa_handler = sigaction_handler;
+    act.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &act, NULL) == -1) {
+        fprintf(stderr, "Error at sigaction()\n");
+        exit(-1);
+    }
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    if (sigprocmask(SIG_BLOCK, &mask, &old_mask) < 0) {
+        fprintf(stderr, "Error at sigprocmask()\n");
+        exit(-1);
+    }
+
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     if (getaddrinfo("ar-net.mykeenetic.net", PORT, &hints, &res) != 0) {
         fprintf(stderr, "Error at getaddrinfo()\n");
         close(client);
-        return;
     }
 
     server = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (server == -1) {
         fprintf(stderr,"Error at socket()\n");
         close(client);
-        return;
     }
 
     if (connect(server, res->ai_addr, res->ai_addrlen) == -1) {
         fprintf(stderr, "Error at connect()\n");
         close(client);
-        return;
     }
 
     if (client > server) {
@@ -72,8 +87,8 @@ void handle(int client) {
         FD_ZERO(&set);
         FD_SET(client, &set);
         FD_SET(server, &set);
-        if (select(max_sock + 1, &set, NULL, NULL, NULL) == -1) {
-            perror("select");
+        if (pselect(max_sock + 1, &set, NULL, NULL, NULL, &old_mask) == -1) {
+            fprintf(stderr, "Error at select()\n");
             break;
         }
         if (FD_ISSET(client, &set)) {
@@ -89,7 +104,6 @@ void handle(int client) {
 
 int main() {
     int sock;
-    struct sigaction signal_action;
     struct addrinfo hints, *res;
     int reuseaddr = 1;
 
@@ -98,58 +112,48 @@ int main() {
     hints.ai_socktype = SOCK_STREAM;
     if (getaddrinfo(NULL, LISTEN_PORT, &hints, &res) != 0) {
         fprintf(stderr, "Error at getaddrinfo()\n");
-        return 1;
+        return -1;
     }
 
-    /* Create the socket */
     sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sock == -1) {
         fprintf(stderr, "Error at socket()\n");
         freeaddrinfo(res);
-        return 1;
+        return -1;
     }
 
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1) {
         fprintf(stderr, "Error at setsockopt()\n");
         freeaddrinfo(res);
-        return 1;
+        return -1;
     }
 
     if (bind(sock, res->ai_addr, res->ai_addrlen) == -1) {
         fprintf(stderr, "Error at bind()\n");
         freeaddrinfo(res);
-        return 1;
+        return -1;
     }
 
     if (listen(sock, LISTEN) == -1) {
         fprintf(stderr, "Error at listen()\n");
         freeaddrinfo(res);
-        return 1;
+        return -1;
     }
 
     freeaddrinfo(res);
 
-    signal_action.sa_handler = wait_for_child;
-    sigemptyset(&signal_action.sa_mask);
-    signal_action.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &signal_action, NULL) == -1) {
-        fprintf(stderr, "Error at sigaction()\n");
-        return 1;
-    }
     while (1) {
         struct sockaddr_in their_addr;
         socklen_t size = sizeof(struct sockaddr_in);
         int newsock = accept(sock, (struct sockaddr *) &their_addr, &size);
-        int pid;
-
         if (newsock == -1) {
             fprintf(stderr, "Error at accept()\n");
-            return 1;
+            return -1;
         } else {
             printf("Got a connection from %s on port %d\n",
                    inet_ntoa(their_addr.sin_addr), htons(their_addr.sin_port));
         }
-        pid = fork();
+        int pid = fork();
         if (pid == 0) {
             close(sock);
             handle(newsock);
@@ -157,7 +161,7 @@ int main() {
         } else {
             if (pid == -1) {
                 fprintf(stderr, "Error at fork()\n");
-                return 1;
+                return -1;
             } else {
                 close(newsock);
             }
